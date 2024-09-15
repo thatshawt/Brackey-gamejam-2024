b97ffbc8a80576659e1992f53eb48e24dc8e8220
John-Michael <121062313+jouumi@users.noreply.github.com>
2024-09-15 03:55:06 -0400

    weapon stats
    setting weapon stats

--- scenes/weapon/weapon.gd
+++ scenes/weapon/weapon.gd
@@ -1,13 +1,49 @@
 extends Sprite2D
 class_name Weapon
 
-var weapon_fire_interval := 0.6
-#var weapon_fire_interval := gun_level / 0.6
 enum weapons {Bow, Crossbow, Flintlock, Handgun, Shotgun, Machine_Gun, Rocket_Launcher, Beam_Emitter}
-#var gun_level: int = GlobalScript.game_state.upgrades_state.get_upgrade_level(Upgrades.UpgradeType.Weapon_Next_Gun)
+var gun_level: int = GlobalScript.game_state.upgrades_state.get_upgrade_level(Upgrades.UpgradeType.Weapon_Next_Gun)
+var fire_interval : float
+var bullet_damage : float
+var bullet_max_hits : int
 
-#func _physics_process(delta):
-	#print(gun_level)
+func _ready():
+	set_stats()
+
+func set_stats():
+	match gun_level:
+		1: #Bow
+			fire_interval = 1.8
+			bullet_damage = 20
+			bullet_max_hits = 1
+		2: #Crossbow
+			fire_interval = 1.0
+			bullet_damage = 20
+			bullet_max_hits = 1
+		3: #Flintlock
+			fire_interval = 0.6
+			bullet_damage = 20
+			bullet_max_hits = 2
+		4: #Handgun
+			fire_interval = 0.3
+			bullet_damage = 25
+			bullet_max_hits = 4
+		5: #Shotgun
+			fire_interval = 0.4
+			bullet_damage = 88
+			bullet_max_hits = 3
+		6: #Machine_Gun
+			fire_interval = 0.1
+			bullet_damage = 25
+			bullet_max_hits = 4
+		7: #Rocket_Launcher
+			fire_interval = 0.8
+			bullet_damage = 148
+			bullet_max_hits = 1
+		8: #Beam_Emitter
+			fire_interval = 0.0
+			bullet_damage = 248
+			bullet_max_hits = 108
 
 func shoot_mech():
 	const BULLET = preload("res://scenes/player/bullet.tscn")
@@ -17,5 +53,5 @@ func shoot_mech():
 	$Marker2D.add_child(new_bullet)
 	
 	GlobalScript.game_state.the_player.attacking = true
-	await get_tree().create_timer(weapon_fire_interval).timeout
+	await get_tree().create_timer(fire_interval).timeout
 	GlobalScript.game_state.the_player.attacking = false
